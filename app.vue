29dc11f0f3b0a49f9616d9cd8dee344bfb766403
Yassin97440 <yassin.abdulla@compufirst.com>
2023-02-07 00:08:01 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLayout>
+    some page content
+  </NuxtLayout>
+  <div>
+    <v-card class="mx-auto" width="400" prepend-icon="mdi-home">
+      <v-card-text>
+        When you see this inside a card, it worked!
+      </v-card-text>
+    </v-card>
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        Some default layout shared across all pages
+        <slot />
+        <v-app-bar title="Application bar"></v-app-bar>
+    </div>
+</template>
