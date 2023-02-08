cd74a0d4cbea1119cab1dc41eb60d9d17091206c
Yassin97440 <yassin.abdulla@compufirst.com>
2023-02-08 23:31:03 +0100

    essai ajout theme custom + mdi : ca marche pas!

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,10 +1,40 @@
 // plugins/vuetify.ts
 
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
+
+const myCustomLightTheme = {
+    dark: false,
+    colors: {
+        background: '#AAFFFF',
+        surface: '#BBD6FF',
+        primary: '#AA00EE',
+        'primary-darken-1': '#3700B3',
+        secondary: '#03DAC6',
+        'secondary-darken-1': '#018786',
+        error: '#B00020',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    }
+}
 export default defineNuxtPlugin(nuxtApp => {
     const vuetify = createVuetify({
         // your config will come here
+        theme: {
+            defaultTheme: 'dark',
+            themes: {
+                myCustomLightTheme
+            }
+        },
+        icons: {
+            defaultSet: 'mdi',
+            aliases,
+            sets: {
+                mdi,
+            }
+        },
     })
 
     nuxtApp.vueApp.use(vuetify)
