19f1e1d6e487d0116eab7da8199d1bdeb0921ebd
Yassin97440 <yassin.abdulla@compufirst.com>
2023-02-20 00:30:27 +0100

    post albums and pictures : OK

--- stores/AlbumStore.js
+++ stores/AlbumStore.js
@@ -13,11 +13,14 @@ export const useAlbumStore = defineStore("albumStore", {
   actions: {
     async postNewAlbums(data) {
       console.log(data.album);
-      const newAlbumResponse = await $fetch("http://localhost:8080/album/new", {
-        headers: { "Content-Type": "application/json" },
-        method: "POST",
-        body: data.album,
-      });
+      const newAlbumResponse = await $fetch(
+        "http://159.89.0.150:8080/album/new",
+        {
+          headers: { "Content-Type": "application/json" },
+          method: "POST",
+          body: data.album,
+        }
+      );
 
       console.log("responseNewAlbum : ", newAlbumResponse);
       const formdata = new FormData();
@@ -26,12 +29,11 @@ export const useAlbumStore = defineStore("albumStore", {
         formdata.append("pictures", pic);
       });
 
-      //   formdata.append("albumName", data.album.name);
+      formdata.append("albumName", data.album.name);
 
       const newPicResponse = await $fetch(
         "http://localhost:8080/pictures/writeAndSavePictures",
         {
-          headers: { "Content-Type": "multipart/form-data" },
           method: "POST",
           body: formdata,
         }
